d4dd26befefea238450dd6da870b3822231efa99
pkyh2 <pkyh1@naver.com>
2021-11-04 18:13:18 +0900

    With AI append

--- bitcoinAutoTradeWithAI.py
+++ bitcoinAutoTradeWithAI.py
@@ -4,8 +4,8 @@ import datetime
 import schedule
 from fbprophet import Prophet
 
-access = "TlKLmK7jp8apGBVlKLbYyrQJjpJJsK2MJc1ZSNSC"
-secret = "AefE6iuSNwdyEvP5a85d2B4TkO3Fhbx0iXpCLapw"
+access = "your-access"
+secret = "your-secret"
 
 def get_target_price(ticker, k):
     """변동성 돌파 전략으로 매수 목표가 조회"""
@@ -18,12 +18,6 @@ def get_start_time(ticker):
     df = pyupbit.get_ohlcv(ticker, interval="day", count=1)
     start_time = df.index[0]
     return start_time
-
-def get_ma5(ticker):
-    """5일 이동 평균선 조회"""
-    df = pyupbit.get_ohlcv(ticker, interval="day", count=5)
-    ma5 = df['close'].rolling(5).mean().iloc[-1]
-    return ma5
 
 def get_balance(ticker):
     """잔고 조회"""
@@ -58,8 +52,8 @@ def predict_price(ticker):
         closeDf = forecast[forecast['ds'] == data.iloc[-1]['ds'].replace(hour=9)]
     closeValue = closeDf['yhat'].values[0]
     predicted_close_price = closeValue
-predict_price("KRW-MANA")
-schedule.every().hour.do(lambda: predict_price("KRW-MANA"))
+predict_price("KRW-BTC")
+schedule.every().hour.do(lambda: predict_price("KRW-BTC"))
 
 # 로그인
 upbit = pyupbit.Upbit(access, secret)
@@ -68,23 +62,22 @@ print("autotrade start")
 # 자동매매 시작
 while True:
     try:
-        now = datetime.datetime.now()                                   # 현재시간
-        start_time = get_start_time("KRW-MANA")                         # 투자시작시간
-        end_time = start_time + datetime.timedelta(days=1)              # 투자종료시간
+        now = datetime.datetime.now()
+        start_time = get_start_time("KRW-BTC")
+        end_time = start_time + datetime.timedelta(days=1)
+        schedule.run_pending()
 
-        # 9:00 < 현재 < 8:59:50
         if start_time < now < end_time - datetime.timedelta(seconds=10):
-            target_price = get_target_price("KRW-MANA", 0.5)             # 목표가 설정
-            # ma5 = get_ma5("KRW-MANA")
-            current_price = get_current_price("KRW-MANA")                # 현재가 확인
-            if target_price < current_price and current_price < predicted_close_price:                            # 목표가보다 현재가가 높으면
-                krw = get_balance("KRW")                                # 내 잔고를 조회하고
-                if krw > 5000:                                          # 잔고가 5000원 이상이면
-                    upbit.buy_market_order("KRW-MANA", krw*0.9995)       # 코인매수 인자값으로 비트코인값과, 잔고 * 수수로 0.05%
+            target_price = get_target_price("KRW-BTC", 0.5)
+            current_price = get_current_price("KRW-BTC")
+            if target_price < current_price and current_price < predicted_close_price:
+                krw = get_balance("KRW")
+                if krw > 5000:
+                    upbit.buy_market_order("KRW-BTC", krw*0.9995)
         else:
-            mana = get_balance("MANA")                                    # 현재 mana잔고 체크
-            if mana > 5000:                                           # 0.00008mana이상이면 전량 매도
-                upbit.sell_market_order("KRW-MANA", mana*0.9995)
+            btc = get_balance("BTC")
+            if btc > 0.00008:
+                upbit.sell_market_order("KRW-BTC", btc*0.9995)
         time.sleep(1)
     except Exception as e:
         print(e)
